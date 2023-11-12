d3f73f198dae41ae25ec1f934722c55baa7282de
Thái Bảo Long <subin27102003@gmail.com>
2023-11-12 17:57:19 +0700

    Xây dựng UI phần Content#1

--- src/Components/Contents/index.js
+++ src/Components/Contents/index.js
@@ -3,6 +3,7 @@ import styles from "./Contents.module.scss";
 import Print from "../images/print.svg";
 import History from "../images/history.svg";
 import Buy from "../images/buy.svg";
+import { Link } from "react-router-dom";
 const cx = classNames.bind(styles);
 function Contents() {
   return (
@@ -16,12 +17,27 @@ function Contents() {
         <div className={cx("option")}>
           <button className={cx("print")}>
             <img src={Print} alt=""></img>
+            <p>
+              <Link to={"/print"} className={cx("custom-link")}>
+                In tài liệu
+              </Link>
+            </p>
           </button>
           <button className={cx("history")}>
             <img src={History} alt=""></img>
+            <p>
+              <Link to={"/history"} className={cx("custom-link")}>
+                Xem lịch sử in
+              </Link>
+            </p>
           </button>
           <button className={cx("buy")}>
             <img src={Buy} alt=""></img>
+            <p>
+              <Link to={"/buy"} className={cx("custom-link")}>
+                Mua thêm trang in
+              </Link>
+            </p>
           </button>
         </div>
       </div>
