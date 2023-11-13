64c6d2f7a7d48d0e7075140ac5abf74308353b81
hathongthuong2k3 <137924980+hathongthuong2k3@users.noreply.github.com>
2023-11-13 15:02:42 +0700

    Xây dựng UI trang Upload tài liệu #1

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,10 @@
 //Route đưa người dùng tới các trang khi chưa đăng nhập
 import Home from "~/Components/Home";
-export const publicRoutes = [{ path: "/", component: Home }];
+import Print from "~/Components/Print";
+export const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/print', component: Print},
+];
 
 //Route đưa người dùng tới các trang khi đã đăng nhập
 export const privateRoutes = [];
